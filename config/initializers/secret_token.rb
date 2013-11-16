8e34545f10d39b7c689930dacb02f3837b5ebca9
Cihad Paksoy <cihadp@gmail.com>
2013-11-16 15:24:21 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ilan::Application.config.secret_key_base = '53838068ac5cae0572920404fb8a095aabceb7b34160d286da4f378d253c5204c2a3de8e7224e3955a286adf5853d82de500e48798191d0790a617036731e107'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ilan::Application.config.session_store :cookie_store, key: '_ilan_session'
