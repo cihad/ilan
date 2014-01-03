a4c1a8497f87f5cbdac63c098697f20ca3337601
Cihad Paksoy <cihadp@gmail.com>
2014-01-03 03:23:03 +0200

    application configuration init

--- app/controllers/control/base_controller.rb
+++ app/controllers/control/base_controller.rb
@@ -2,7 +2,8 @@ module Control
   class BaseController < ApplicationController
     before_action :set_default_breadcrumb
     layout "control"
-    http_basic_authenticate_with name: "admin", password: "secret"
+    http_basic_authenticate_with  name:     ENV['CONTROL_USERNAME'],
+                                  password: ENV["CONTROL_PASSWORD"]
 
     def index
     end
